c7ee63a123d6d3d71acc4e69e256c5415993dfea
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-07-25 23:39:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", function(req, res) {
+    res.render("home", {
+        title: "Hello World",
+        body: "This is the body of the the homepage",
+    });
+});
 
 app.get("/another", (req, res) => res.send("Another World!"));
 
