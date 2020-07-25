b7c0fef56b928c5d3fd607244b953a1376ba3ee3
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-07-26 00:02:42 +0200

    home not found

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
-app.engine("handlebars", exphbs({ extname: ".html" }));
+app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 const port = 3000;
 
