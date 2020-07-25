481243f38ce1b599d22d7b52c560b63ddfc5e6bd
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-07-26 00:20:28 +0200

    It's working now

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
-app.engine("handlebars", exphbs());
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 const port = 3000;
 
